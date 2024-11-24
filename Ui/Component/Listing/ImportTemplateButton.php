8276d558029d888b628066a51ad9d9ee9274a04f
dadolun95 <dadolun@gmail.com>
2024-11-24 16:50:03 +0100

    Init Admin ui for template import

---
+++ Ui/Component/Listing/ImportTemplateButton.php
@@ -0,0 +1,46 @@
+<?php
+declare(strict_types=1);
+
+namespace MageOS\PageBuilderTemplateImportExport\Ui\Component\Listing;
+
+use Magento\Framework\AuthorizationInterface;
+use Magento\Framework\View\Element\UiComponent\Control\ButtonProviderInterface;
+
+class ImportTemplateButton implements ButtonProviderInterface
+{
+
+    const ACL_PAGEBUILDER_IMPORT_TEMPLATES = 'MageOS_PageBuilderTemplateImportExport::pagebuilder_template_import';
+
+    /**
+     * @param AuthorizationInterface $authorization
+     */
+    public function __construct(
+        private readonly AuthorizationInterface $authorization
+    ) {
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getButtonData()
+    {
+        if (!$this->isAllowed()) {
+            return [];
+        }
+
+        return [
+            'label' => __('Import Template'),
+            'sort_order' => '100',
+            'class' => 'primary',
+            'on_click' => 'jQuery(".mage-os-pagebuilder-template-import-modal").trigger("openModal");'
+        ];
+    }
+
+    /**
+     * Verify if  Adobe Stock Search button allowed
+     */
+    private function isAllowed(): bool
+    {
+        return $this->authorization->isAllowed(self::ACL_PAGEBUILDER_IMPORT_TEMPLATES);
+    }
+}
